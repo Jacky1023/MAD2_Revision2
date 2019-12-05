1eff3020d9ccd7fcd03e4d65dd019359640c3cc6
Wong Kah Wo <s10189113@connct.np.edu.sg>
2019-12-05 17:14:32 +0800

    part 1 done

---
+++ MAD2_Revision2/RecipeTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeTableViewController.swift
+//  MAD2_Revision2
+//
+//  Created by MAD2_P02 on 5/12/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RecipeTableViewController : UITableViewController{
+    
+}
