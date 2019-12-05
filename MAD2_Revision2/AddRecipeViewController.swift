156bdf49a276b8f7882128286dc3a864db4a6a23
Wong Kah Wo <s10189113@connct.np.edu.sg>
2019-12-05 17:59:15 +0800

    part2 ci done

---
+++ MAD2_Revision2/AddRecipeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddRecipeViewController.swift
+//  MAD2_Revision2
+//
+//  Created by MAD2_P02 on 5/12/19.
+//  Copyright © 2019 MAD2_P02. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddRecipeViewController : UIViewController{
+    
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtPreparationTime: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var txtIngredient1: UITextField!
+    @IBOutlet weak var txtIngredient2: UITextField!
+    @IBOutlet weak var txtIngredient3: UITextField!
+    @IBOutlet weak var txtIngredient4: UITextField!
+    @IBOutlet weak var txtIngredient5: UITextField!
+    
+    @IBAction func btnAdd(_ sender: Any) {
+        if(txtTitle.text == "" && txtPreparationTime.text == "" ){
+            
+            let alert = UIAlertController(title: "Empty Field", message: "Please populate the title and preparation time", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Noted", style: .default))
+            self.present(alert, animated: true, completion: nil)
+        }
+
+        
+    }
+    
+    
+}
